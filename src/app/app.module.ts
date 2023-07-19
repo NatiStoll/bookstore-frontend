4a6d224f0c9fe293f8ff7525ac9be31c8d1e5d20
Ramon Carvalho <ramon@DESKTOP-F973C7O>
2023-07-19 08:02:08 -0300

    tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './modules/auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+import {MatButtonModule} from '@angular/material/button';
 // import { HeaderComponent } from './shared/components/header/header.component';
 // import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, AuthModule],
+  imports: [BrowserModule,
+            AppRoutingModule,
+            SharedModule,
+            AuthModule,
+            MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
